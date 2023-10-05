aea895c7ae2a8548159514944b4a8f556c6a8395
biamila <biancamarcellomilanez@hotmail.com>
2023-10-05 17:06:04 +0100

    Solve prime v2 challenge

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
+    counter = 2
+    while len(list) < number_of_primes:
+        divisible = False
+
+        for i in range(counter, 1, -1):
+            if counter%i==0 and counter != i:
+                divisible=True
+                break
+
+        if (not divisible):
+            list.append(counter)
+
+        counter+= 1
+        
     return list
